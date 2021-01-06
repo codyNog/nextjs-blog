14158002527d47abdd1ce7db3f394099997a3ac9
codyNog <yoneyoneyone99@gmail.com>
2021-01-06 15:56:37 +0900

    eslint設定

--- src/data/apis/fetchBlogPost/index.ts
+++ src/data/apis/fetchBlogPost/index.ts
@@ -1,6 +1,5 @@
-import { CMS_API_KEY, CMS_ROOT } from "~/constants/apis";
 import { Blog } from "~/types/blog";
-import { CMSClient } from "../CMSClient";
+import { CMSClient } from "~/data/apis/CMSClient";
 
 export const fetchBlogPost = async (id: string): Promise<Blog> => {
   const res = await CMSClient(`/blog/${id}`);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title key={"title"}>codyNog's Labo</title>
+        <title key={"title"}>codyNog&apos;s Labo</title>
         <meta name="description" content={"codyNog's Labo"} />
       </Head>
       <Component {...pageProps} />
