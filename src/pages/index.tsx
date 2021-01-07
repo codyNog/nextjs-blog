7f2b9f80f8b3d562d641e3f3cfc7a5a136cbcb65
codyNog <yoneyoneyone99@gmail.com>
2021-01-07 12:28:21 +0900

    lint修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, StyledStepper, Tag } from "@chakra-ui/react";
+import { Box, Flex, Tag } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import dayjs from "dayjs";
 import Link from "next/link";
