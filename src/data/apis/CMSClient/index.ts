7a8850c684d2f93df319e2a61587375b42251701
codyNog <yoneyoneyone99@gmail.com>
2021-02-01 02:14:35 +0900

    CI修正

--- src/data/apis/CMSClient/index.ts
+++ src/data/apis/CMSClient/index.ts
@@ -4,8 +4,6 @@ export const CMSClient = async (uri: string) => {
   const key = {
     headers: { "X-API-KEY": CMS_API_KEY },
   };
-
-  console.log(`${CMS_ROOT}${uri}`);
   const res = await (await fetch(`${CMS_ROOT}${uri}`, key)).json();
   return res;
 };
