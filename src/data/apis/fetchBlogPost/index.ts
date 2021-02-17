c66fbfb34f564f218c6075e626395e2abe4202bc
codyNog <yoneyoneyone99@gmail.com>
2021-02-18 04:52:51 +0900

    記事のhtmlstringを整形

--- src/data/apis/fetchBlogPost/index.ts
+++ src/data/apis/fetchBlogPost/index.ts
@@ -1,8 +1,11 @@
 import { Blog } from "~/types/blog";
 import { CMSClient } from "~/data/apis/CMSClient";
+import module from "./module";
 
 export const fetchBlogPost = async (id: string): Promise<Blog> => {
-  const res = await CMSClient(`/blog/${id}`);
+  const res: Blog = await CMSClient(`/blog/${id}`);
 
-  return res;
+  const data = { ...res, body: module.htmlConverter(res.body) };
+
+  return data;
 };
